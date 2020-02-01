563f7401a4e36f130a75f463ba7ddfa97c003377
Nicolò Zannini <nikisa97@gmail.com>
2020-02-01 12:50:35 +0100

    Knight Movement

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -5,6 +5,43 @@ using UnityEngine;
 public class KnightController : MonoBehaviour
 {
 
+    //Inspector
+    #region data
+    public GameObject Target;
+    public float MaxSpeed;
+    public float TimeAcceleration;
+    public float DynamicDrag;
+    [Range(0,1)]
+    public float DeadZone;
+    #endregion
+
+
+    //Public 
+    //[HideInInspector]
+    public float MoveSpeed;
+    [HideInInspector]
+    public Vector3 Inertia;
+    [HideInInspector]
+    public Vector3 OldPos;
+    [HideInInspector]
+    public Vector3 VectorAngle;
+
+    //Private
+    private float AnalogAngle;
+    //private Vector3 startPosition;
+
+
+    #region Inputs
+    string HKC1 = "HorizontalKnightController1";
+    string HKC2 = "HorizontalKnightController2";
+    string VKC1 = "VerticalKnightController1";
+    string VKC2 = "VerticalKnightController2";
+    string HSC1 = "HorizontalSlaveController1";
+    string HSC2 = "HorizontalSlaveController2";
+    string VSC1 = "VerticalSlaveController1";
+    string VSC2 = "VerticalSlaveController2";
+    #endregion
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +52,33 @@ public class KnightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 
     public void Move()
     {
         
+        if (Mathf.Pow(Input.GetAxis(HKC1),2) + Mathf.Pow(Input.GetAxis(VKC1), 2) > Mathf.Pow(DeadZone,2)) {
+            Debug.LogFormat("Horizontal: {0} --- Vertical: {1}", Input.GetAxis(HKC1), Input.GetAxis(VKC1));
+            Rotate();
+            MoveSpeed = (MaxSpeed / TimeAcceleration * Time.deltaTime );
+            DynamicDrag = (MaxSpeed - MoveSpeed) / MaxSpeed;
+            VectorAngle = Target.transform.position - transform.position;
+            OldPos = transform.position;
+            transform.position = transform.position + Inertia + MoveSpeed * VectorAngle.normalized * Time.deltaTime;
+            Inertia = (transform.position - OldPos) * (DynamicDrag);
+        }
+        else {
+            MoveSpeed = 0;
+        }
+    }
+
+    public void Rotate() {
+        Vector3 inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis(HKC1);
+        inputDirection.z = Input.GetAxis(VKC1);
+        Target.transform.position = transform.position + inputDirection;
+
+        //Target.transform.RotateAround(transform.position, new Vector3(0, 1, 0), AnalogAngle * Time.deltaTime);
     }
 }
