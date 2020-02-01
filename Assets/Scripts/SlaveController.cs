fb0d2a07e63c6526ffdeca91df68c69bd798c0e6
Nicolò Zannini <nikisa97@gmail.com>
2020-02-01 15:59:25 +0100

    Slave + Knight Movement (BOTH CONTROLLERS)

--- Assets/Scripts/SlaveController.cs
+++ Assets/Scripts/SlaveController.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class KnightController : MonoBehaviour
+public class SlaveController : MonoBehaviour
 {
-    
-    public enum PlayerSelection{Player1, Player2}
+
+    public enum PlayerSelection { Player1, Player2 }
 
     //Inspector
-    #region data
     public PlayerSelection MyPlayerSelection;
     public GameObject Target;
     public float MaxSpeed;
     public float TimeAcceleration;
     public float DynamicDrag;
-    [Range(0,1)]
+    public float RotationSpeed;
+    [Range(0, 1)]
     public float DeadZone;
-    #endregion
 
-
-    //Public 
+    //Public
     [HideInInspector]
     public float MoveSpeed;
     [HideInInspector]
@@ -29,28 +28,29 @@ public class KnightController : MonoBehaviour
     [HideInInspector]
     public Vector3 VectorAngle;
 
+
     //Private
-    private float AnalogAngle;
-    //private Vector3 startPosition;
+    CharacterController characterController;
+    Vector3 AxisDirection;
+    Vector3 moveDirection = Vector3.zero;
 
-
-    #region Inputs
-    string HKC1 = "HorizontalKnightController1";
-    string HKC2 = "HorizontalKnightController2";
-    string VKC1 = "VerticalKnightController1";
-    string VKC2 = "VerticalKnightController2";
+    #region inputs
+    string HSC1 = "HorizontalSlaveController1";
+    string HSC2 = "HorizontalSlaveController2";
+    string VSC1 = "VerticalSlaveController1";
+    string VSC2 = "VerticalSlaveController2";
     #endregion
 
-
-    void Update()
-    {
-        Move();
+    void Update() {
+        CheckInput();
     }
 
-    public void Move()
-    {
+
+
+    public void Movement() {
+
         if (MyPlayerSelection == PlayerSelection.Player1) {
-            if (Mathf.Pow(Input.GetAxis(HKC1), 2) + Mathf.Pow(Input.GetAxis(VKC1), 2) > Mathf.Pow(DeadZone, 2)) {
+            if (Mathf.Pow(Input.GetAxis(HSC1), 2) + Mathf.Pow(Input.GetAxis(VSC1), 2) > Mathf.Pow(DeadZone, 2)) {
                 Rotate();
                 MoveSpeed = (MaxSpeed / TimeAcceleration * Time.deltaTime);
                 DynamicDrag = (MaxSpeed - MoveSpeed) / MaxSpeed;
@@ -64,7 +64,7 @@ public class KnightController : MonoBehaviour
             }
         }
         else if (MyPlayerSelection == PlayerSelection.Player2) {
-            if (Mathf.Pow(Input.GetAxis(HKC2), 2) + Mathf.Pow(Input.GetAxis(VKC2), 2) > Mathf.Pow(DeadZone, 2)) {
+            if (Mathf.Pow(Input.GetAxis(HSC2), 2) + Mathf.Pow(Input.GetAxis(VSC2), 2) > Mathf.Pow(DeadZone, 2)) {
                 Rotate();
                 MoveSpeed = (MaxSpeed / TimeAcceleration * Time.deltaTime);
                 DynamicDrag = (MaxSpeed - MoveSpeed) / MaxSpeed;
@@ -78,25 +78,51 @@ public class KnightController : MonoBehaviour
             }
         }
 
-        
+
+    }
+
+
+    public void SlaveRotation() {
+        transform.LookAt(Target.transform.position);
     }
 
     public void Rotate() {
 
         if (MyPlayerSelection == PlayerSelection.Player1) {
             Vector3 inputDirection = Vector3.zero;
-            inputDirection.x = Input.GetAxis(HKC1);
-            inputDirection.z = Input.GetAxis(VKC1);
+            inputDirection.x = Input.GetAxis(HSC1);
+            inputDirection.z = Input.GetAxis(VSC1);
             Target.transform.position = transform.position + inputDirection;
         }
         else if (MyPlayerSelection == PlayerSelection.Player2) {
             Vector3 inputDirection = Vector3.zero;
-            inputDirection.x = Input.GetAxis(HKC2);
-            inputDirection.z = Input.GetAxis(VKC2);
+            inputDirection.x = Input.GetAxis(HSC2);
+            inputDirection.z = Input.GetAxis(VSC2);
             Target.transform.position = transform.position + inputDirection;
         }
 
+    }
 
-        
+    public void CheckInput() {
+
+        if (MyPlayerSelection == PlayerSelection.Player1) {
+            if (Mathf.Pow(Input.GetAxis(HSC1), 2) + Mathf.Pow(Input.GetAxis(VSC1), 2) > Mathf.Pow(DeadZone, 2)) {
+                Debug.Log("moveDirection: " + moveDirection);
+                Rotate();
+                Movement();
+                SlaveRotation();
+            }
+        }
+        else if (MyPlayerSelection == PlayerSelection.Player2) {
+            if (Mathf.Pow(Input.GetAxis(HSC2), 2) + Mathf.Pow(Input.GetAxis(VSC2), 2) > Mathf.Pow(DeadZone, 2)) {
+                Debug.Log("moveDirection: " + moveDirection);
+                Rotate();
+                Movement();
+                SlaveRotation();
+            }
+
+
+        }
+
     }
 }
