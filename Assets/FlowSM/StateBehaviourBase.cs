4943b39b1e233994e41a44582936f02d04c9b1e7
GabrieleAncona <gabry.ancona@gmail.com>
2020-02-01 10:09:33 +0100

    implementata flowSM

---
+++ Assets/FlowSM/StateBehaviourBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class StateBehaviourBase : StateMachineBehaviour
+{
+    protected GameManager.Context ctx = new GameManager.Context();
+
+    public void Setup(GameManager.Context _ctx)
+    {
+
+        ctx = _ctx;
+    }
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OnEnter();
+    }
+    public virtual void OnEnter() { }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OnUpdate();
+    }
+
+    public virtual void OnUpdate() { }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OnExit();
+    }
+    public virtual void OnExit() { }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public Context newContext;
+    public Animator animController;
+
+    void Awake()
+    {
+        Singleton();
+        SetUpSM();
+    }
+
+    public void Singleton()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public class Context { }
+
+    void SetUpSM()
+    {
+        //Setup della state machine e del context
+        animController = GetComponent<Animator>();
+
+        Context context = new Context()
+        {
+
+        };
+        foreach (StateBehaviourBase state in animController.GetBehaviours<StateBehaviourBase>())
+        {
+            state.Setup(context);
+        }
+    }
+
+    public static Action GoToMainMenu;
+    public static Action GoToPause;
+    public static Action GoToFigthFase;
+    public static Action GoToRepairFase;
+
+
+    //events
+    private void HandleOnMainMenu()
+    {
+        animController.SetTrigger("GoToMainMenu");
+    }
+
+    private void HandleOnFightFase()
+    {
+        animController.SetTrigger("GoToFightFase");
+    }
+
+    private void HandleOnRepairFase()
+    {
+        animController.SetTrigger("GoToRepairFase");
+    }
+
+    private void HandleOnPause()
+    {
+        animController.SetTrigger("GoToPause");
+    }
+
+    private void OnDisable()
+    {
+        if (instance == this)
+        {
+            GoToMainMenu -= instance.HandleOnMainMenu;
+            GoToPause -= instance.HandleOnPause;
+            GoToFigthFase -= instance.HandleOnFightFase;
+            GoToRepairFase -= instance.HandleOnRepairFase;
+
+        }
+    }
+}
