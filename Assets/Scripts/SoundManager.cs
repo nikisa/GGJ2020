e44b82f465f32f7eca52ed1e57ad272a38c64bdc
GabrieleAncona <gabry.ancona@gmail.com>
2020-02-01 11:21:29 +0100

    Creato soundManager e game assets

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+    public enum Sound
+    {
+
+    }
+    public void PlaySound(Sound sound)
+    {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
+    private AudioClip GetAudioClip(Sound sound)
+    {
+        foreach (GameAssets.SoundAudioClip soundAudioClip in GameAssets.i.soundAudioClipArray)
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+
+        }
+        return null;
+    }
+}
